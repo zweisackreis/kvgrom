a948070da8e502f8c5b61900f9dce04a5ac5c074
zweisackreis <sebastian.musli@kvgschulen.de>
2026-05-29 06:09:12 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cache-Version — wird bei jedem Update automatisch hochgezählt
-const CACHE = 'kvgrom-v20260528-1849';
+const CACHE = 'kvgrom-v20260528-1859';
 const ASSETS = ['./', './index.html', './manifest.json', './logo-fahrt.jpeg', './logo-schule.png', './hero-bg.jpg'];
 
 // Installation: neuen Cache befüllen
